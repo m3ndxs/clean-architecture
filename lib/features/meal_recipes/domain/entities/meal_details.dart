5c645c8619d279513b214b8f981bdcf791471757
Leonardo Mendes <leonardo_mendes2002@hotmail.com>
2026-05-09 11:10:33 -0300

    Adicionando entidades

---
+++ lib/features/meal_recipes/domain/entities/meal_details.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+import 'package:meal_explorer/features/meal_recipes/domain/entities/ingredient.dart';
+
+class MealDetails extends Equatable {
+  final String id;
+  final String name;
+  final String imageUrl;
+  final List<Ingredient> ingredients;
+  final String instructions;
+
+  const MealDetails({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.ingredients,
+    required this.instructions,
+  });
+
+  @override
+  List<Object?> get props => [id, name, imageUrl, ingredients, instructions];
+}
