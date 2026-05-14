c4d86517183253f9daecc50f9091cc3fb8a31ede
Leonardo Mendes <leonardo_mendes2002@hotmail.com>
2026-05-14 18:43:38 -0300

    Adicionando datasources

--- lib/features/meal_recipes/data/models/meal_detail_model.dart
+++ lib/features/meal_recipes/data/models/meal_detail_model.dart
@@ -1,4 +1,5 @@
 import 'package:meal_explorer/features/meal_recipes/data/models/ingredient_model.dart';
+import 'package:meal_explorer/features/meal_recipes/domain/entities/ingredient.dart';
 import 'package:meal_explorer/features/meal_recipes/domain/entities/meal_detail.dart';
 
 class MealDetailModel extends MealDetail {
@@ -10,9 +11,17 @@ class MealDetailModel extends MealDetail {
     required super.instructions,
   });
 
-  factory MealDetailModel.fromRemoteJson(Map<String, dynamic> json) {
-    final List<IngredientModel>? ingredientModel = extractIngredients(json);
+  static List<MealDetailModel> fromRemoteJsonList(List<dynamic> jsonList) {
+    return jsonList
+        .map((json) => MealDetailModel.fromRemoteJson(json))
+        .toList();
+  }
 
+  // Convertendo JSON da API para objeto Dart
+  factory MealDetailModel.fromRemoteJson(Map<String, dynamic> json) {
+    final List<Ingredient>? ingredientModel = extractIngredients(json);
+
+    // Pega os campos crus do JSON e cria um objeto MealDetailModel fortemente tipado
     return MealDetailModel(
       id: json['idMeal'],
       name: json['strMeal'],
@@ -22,11 +31,14 @@ class MealDetailModel extends MealDetail {
     );
   }
 
+  // Convertendo JSON Local para objeto Dart
   factory MealDetailModel.fromLocalJson(Map<String, dynamic> json) {
     List<dynamic> ingredients = json['ingredients'];
-    final List<IngredientModel> ingredientModel = ingredients
+
+    // Transforma uma lista em outra
+    final List<Ingredient> ingredientModel = ingredients
         .map(
-          (e) => IngredientModel.fromParam(
+          (e) => Ingredient(
             name: e['name'] as String,
             measure: e['measure'] as String,
           ),
@@ -42,8 +54,8 @@ class MealDetailModel extends MealDetail {
     );
   }
 
-  static List<IngredientModel>? extractIngredients(Map<String, dynamic> json) {
-    final List<IngredientModel> result = [];
+  static List<Ingredient>? extractIngredients(Map<String, dynamic> json) {
+    final List<Ingredient> result = [];
 
     for (int i = 1; i <= 20; i++) {
       final ingredientName = json['strIngredient$i'] as String?;
@@ -51,7 +63,7 @@ class MealDetailModel extends MealDetail {
 
       if (ingredientName != null && ingredientName.trim().isNotEmpty) {
         result.add(
-          IngredientModel(name: ingredientName, measure: measure?.trim() ?? ''),
+          Ingredient(name: ingredientName, measure: measure?.trim() ?? ''),
         );
       }
     }
@@ -59,13 +71,16 @@ class MealDetailModel extends MealDetail {
     return result;
   }
 
+  // Caminho Inverso, transforma um objeto em JSON
   Map<String, dynamic> toJson() {
     return {
       'idMeal': id,
       'strMeal': name,
       'strMealThumb': imageUrl,
       'strInstructions': instructions,
-      'ingredients': ingredients.map((e) => (e as IngredientModel).toJson()).toList(),
+      'ingredients': ingredients
+          .map((e) => (e as IngredientModel).toJson())
+          .toList(),
     };
   }
 }
